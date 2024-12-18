bcc322d8e4c3b2efce3d3325ea7340b5f6109735
Choi Hyeok <114526859+Choi-Hyeok99@users.noreply.github.com>
2024-12-18 20:28:19 +0900

    feat : User dto Create

---
+++ src/main/java/com/sparta/haengye_project/dto/UserSignupRequestDto.java
@@ -0,0 +1,14 @@
+package com.sparta.haengye_project.dto;
+
+import lombok.Data;
+
+@Data
+public class UserSignupRequestDto {
+
+    private String email;       // 이메일
+    private String password;    // 비밀번호
+    private String name;        // 이름
+    private String phoneNumber; // 전화번호
+    private String address;     // 주소
+
+}
---
+++ src/main/java/com/sparta/haengye_project/dto/UserSignupResponseDto.java
@@ -0,0 +1,11 @@
+package com.sparta.haengye_project.dto;
+
+import lombok.Data;
+
+@Data
+public class UserSignupResponseDto {
+    private String userId;
+    private String email;
+    private String name;
+
+}
