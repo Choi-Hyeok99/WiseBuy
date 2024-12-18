56e8663a5f42904c3e9f8129ba328745669eeeaf
Choi Hyeok <114526859+Choi-Hyeok99@users.noreply.github.com>
2024-12-18 19:14:59 +0900

    init

---
+++ src/test/java/com/sparta/haengye_project/HaengyeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.haengye_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaengyeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
