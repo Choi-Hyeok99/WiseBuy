524c4fcf160ff9bc2163dce313cd955fd53a3b5c
Choi-Hyeok99 <gur0709@naver.com>
2024-12-18 21:09:27 +0900

    feat : 회원가입 구현

--- src/main/java/com/sparta/haengye_project/user/dto/UserSignupResponseDto.java
+++ src/main/java/com/sparta/haengye_project/user/dto/UserSignupResponseDto.java
@@ -1,10 +1,12 @@
 package com.sparta.haengye_project.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class UserSignupResponseDto {
-    private String userId;
+    private Long userId;
     private String email;
     private String name;
 
--- src/main/java/com/sparta/haengye_project/user/repository/UserRepository.java
+++ src/main/java/com/sparta/haengye_project/user/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.sparta.haengye_project.user.repository;
 
 
-import org.apache.catalina.User;
+
+
+import com.sparta.haengye_project.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
