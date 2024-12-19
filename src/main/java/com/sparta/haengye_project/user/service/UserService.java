8390ad811cb6221857cbfa428f1e10fcc5c9ed2c
Choi Hyeok <114526859+Choi-Hyeok99@users.noreply.github.com>
2024-12-19 16:45:16 +0900

    feat : 비밀번호 변경 service 추가

--- src/main/java/com/sparta/haengye_project/user/service/UserService.java
+++ src/main/java/com/sparta/haengye_project/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.sparta.haengye_project.user.service;
 
 
+import com.sparta.haengye_project.user.dto.ChangePasswordRequestDto;
 import com.sparta.haengye_project.user.dto.UserSignupRequestDto;
 import com.sparta.haengye_project.user.dto.UserSignupResponseDto;
 import com.sparta.haengye_project.user.entity.User;
@@ -47,7 +48,7 @@ public class UserService {
         // 인증 토큰 생성 (랜덤 UUID 사용)
         String token = UUID.randomUUID().toString();
 
-        // 레딧 토큰 저장 ( 24 시간 )
+        // 레딧 토큰 저장
         tokenService.createEmailVerificationToken(requestDto.getEmail(), token);
 
 
@@ -57,6 +58,32 @@ public class UserService {
 
 
         return new UserSignupResponseDto(savedUser.getUserId(), savedUser.getEmail(), savedUser.getName());
+
+    }
+
+    // 비밀번호 변경
+    public void changePassword(Long userId, ChangePasswordRequestDto requestDto) {
+
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isEmpty()){
+            throw new IllegalArgumentException("사용자를 찾을 수 없습니다.");
+        }
+
+        User user = userOptional.get();
+
+        // 2. 현재 비밀번호 검증 ( 기존 비밀번호와 입력된 비밀번호 비교 )
+        if (!passwordEncoder.matches(requestDto.getCurrentPassword(), user.getPassword())) {
+            throw new IllegalArgumentException("현재 비밀번호가 잘못되었습니다.");
+        }
+
+        // 새로운 비밀번호 암호화
+        String encodedNewPassword = passwordEncoder.encode(requestDto.getNewPassword());
+
+        // 4. 새로운 비밀번호로 업데이트
+        user.setPassword(encodedNewPassword);
+
+        // 5. 사용자 정보 업데이트
+        userRepository.save(user);
 
     }
 }
