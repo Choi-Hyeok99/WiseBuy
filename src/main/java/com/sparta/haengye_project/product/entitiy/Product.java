641ad1e50ea677e204ec5a18f0e7bd4168a5b9a8
Choi Hyeok <114526859+Choi-Hyeok99@users.noreply.github.com>
2024-12-21 17:01:27 +0900

    update : id 이름 변경

--- src/main/java/com/sparta/haengye_project/product/entitiy/Product.java
+++ src/main/java/com/sparta/haengye_project/product/entitiy/Product.java
@@ -1,5 +1,7 @@
 package com.sparta.haengye_project.product.entitiy;
 
+import com.sparta.haengye_project.product.dto.ProductRequestDto;
+import com.sparta.haengye_project.product.dto.ProductResponseDto;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,8 +16,8 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false, unique = true)
-    private Long product_id; // 상품번호
+    @Column(name = "product_id" , nullable = false, unique = true)
+    private Long id; // 상품번호
 
     @Column(nullable = false, length = 100)
     private String productName; // 상품이름
@@ -34,4 +36,35 @@ public class Product {
     private ProductInfo productInfo; // 상세 정보와 1:1 관계
 
 
+    // DTO -> Entity 변환
+    public static Product fromRequestDto(ProductRequestDto requestDto){
+        Product product = new Product();
+        product.productName = requestDto.getProductName();
+        product.stock = requestDto.getStock();
+        product.startTime = requestDto.getStartTime();
+        product.endTime = requestDto.getEndTime();
+
+        ProductInfo productInfo = new ProductInfo();
+        productInfo.setPrice(requestDto.getPrice());
+        productInfo.setDescription(requestDto.getDescription());
+        productInfo.setImagePath(requestDto.getImagePath());
+        productInfo.setProduct(product); // 관계 설정
+
+        product.setProductInfo(productInfo); // Product와 관계 설정
+        return product;
+    }
+    // Entity -> DTO 변환
+    public ProductResponseDto toResponseDto() {
+        return new ProductResponseDto(
+                this.id,
+                this.productName,
+                this.stock,
+                this.productInfo != null ? this.productInfo.getPrice() : null,
+                this.productInfo != null ? this.productInfo.getImagePath() : null,
+                this.startTime != null ? this.startTime.toLocalDate()
+                                                       .atStartOfDay() : null,
+                this.endTime != null ? this.endTime.toLocalDate()
+                                                   .atStartOfDay() : null
+        );
+    }
 }
