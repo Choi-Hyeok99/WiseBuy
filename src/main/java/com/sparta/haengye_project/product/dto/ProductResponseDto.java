3d50944995626678e142b5c048fd8e34d76d8dbe
Choi-Hyeok99 <gur0709@naver.com>
2024-12-21 17:01:48 +0900

    feat : ProductDto 생성

--- src/main/java/com/sparta/haengye_project/product/dto/ProductResponseDto.java
+++ src/main/java/com/sparta/haengye_project/product/dto/ProductResponseDto.java
@@ -3,13 +3,14 @@ package com.sparta.haengye_project.product.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class ProductResponseDto {
 
-    private Long productId;
+    private Long id;
     private String productName;
     private Integer stock;
     private Integer price;
@@ -17,15 +18,15 @@ public class ProductResponseDto {
     private String endTime;
     private String imagePath;
 
-    public ProductResponseDto(Long productId, String productName, Integer stock,
+    public ProductResponseDto(Long id, String productName, Integer stock,
                               Integer price,String imagePath,
-                              LocalDate startTime, LocalDate endTime) {
-        this.productId = productId;
+                              LocalDateTime startTime, LocalDateTime endTime) {
+        this.id = id;
         this.productName = productName;
         this.stock = stock;
         this.price = price;
+        this.imagePath = imagePath != null ? imagePath : null; // null 처리
         this.startTime = startTime != null ? startTime.toString() : null;
         this.endTime = endTime != null ? endTime.toString() : null;
-        this.imagePath = imagePath != null ? imagePath : null; // null 처리
     }
 }
