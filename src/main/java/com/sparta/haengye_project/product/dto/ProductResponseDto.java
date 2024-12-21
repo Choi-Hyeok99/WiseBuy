99c9c8a7f46e69c4390ddaf2792d34bca5fd59f5
Choi Hyeok <114526859+Choi-Hyeok99@users.noreply.github.com>
2024-12-21 15:32:36 +0900

    update : imagePath 추가

--- src/main/java/com/sparta/haengye_project/product/dto/ProductResponseDto.java
+++ src/main/java/com/sparta/haengye_project/product/dto/ProductResponseDto.java
@@ -15,9 +15,10 @@ public class ProductResponseDto {
     private Integer price;
     private String startTime;
     private String endTime;
+    private String imagePath;
 
     public ProductResponseDto(Long productId, String productName, Integer stock,
-                              Integer price,
+                              Integer price,String imagePath,
                               LocalDate startTime, LocalDate endTime) {
         this.productId = productId;
         this.productName = productName;
@@ -25,5 +26,6 @@ public class ProductResponseDto {
         this.price = price;
         this.startTime = startTime != null ? startTime.toString() : null;
         this.endTime = endTime != null ? endTime.toString() : null;
+        this.imagePath = imagePath != null ? imagePath : null; // null 처리
     }
 }
