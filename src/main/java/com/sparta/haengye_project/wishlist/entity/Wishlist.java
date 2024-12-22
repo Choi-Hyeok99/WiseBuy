e53b9b85dafcad057688f78313b2c8f94302b0ed
Choi Hyeok <114526859+Choi-Hyeok99@users.noreply.github.com>
2024-12-22 21:59:25 +0900

    refactor: @Getter,@Setter 추가

--- src/main/java/com/sparta/haengye_project/wishlist/entity/Wishlist.java
+++ src/main/java/com/sparta/haengye_project/wishlist/entity/Wishlist.java
@@ -2,11 +2,15 @@ package com.sparta.haengye_project.wishlist.entity;
 
 import com.sparta.haengye_project.user.entity.User;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class Wishlist {
 
     @Id
@@ -17,7 +21,8 @@ public class Wishlist {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @OneToMany(mappedBy = "wishlist",cascade = CascadeType.ALL,orphanRemoval = true)
+    @OneToMany(mappedBy = "wishlist", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<WishListItem> items = new ArrayList<>();
 
 }
+
