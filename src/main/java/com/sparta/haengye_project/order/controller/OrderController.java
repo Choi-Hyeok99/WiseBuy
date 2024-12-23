007ff6d2b8d28bfb223760917435007b05cfecbc
Choi-Hyeok99 <gur0709@naver.com>
2024-12-23 16:40:58 +0900

    feat : 주문 조회 기능 구현

--- src/main/java/com/sparta/haengye_project/order/controller/OrderController.java
+++ src/main/java/com/sparta/haengye_project/order/controller/OrderController.java
@@ -9,10 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("orders")
@@ -34,6 +33,19 @@ public class OrderController {
 
        OrderResponseDto responseDto =  orderService.createOrder(requestDto,user,address);
        return ResponseEntity.ok(responseDto);
+    }
+
+    // 2. 주문 조회
+    @GetMapping
+    public ResponseEntity<List<OrderResponseDto>> getOrders(){
+        // 유저 확인하고 이제 주문
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        User user = userDetails.getUser();
+
+        List<OrderResponseDto> orders = orderService.getOrders(user);
+        return ResponseEntity.ok(orders);
+
     }
 
 }
