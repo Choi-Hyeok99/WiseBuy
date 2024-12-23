a2b412416b9faf017ae0cafa129c8c141eb88da2
Choi Hyeok <114526859+Choi-Hyeok99@users.noreply.github.com>
2024-12-23 10:29:42 +0900

    feat : wishlist 삭제 기능 구현

--- src/main/java/com/sparta/haengye_project/wishlist/repository/WishlistRepository.java
+++ src/main/java/com/sparta/haengye_project/wishlist/repository/WishlistRepository.java
@@ -1,6 +1,7 @@
 package com.sparta.haengye_project.wishlist.repository;
 
 import com.sparta.haengye_project.user.entity.User;
+import com.sparta.haengye_project.wishlist.entity.WishListItem;
 import com.sparta.haengye_project.wishlist.entity.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,5 +12,7 @@ import java.util.Optional;
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
 
     Optional<Wishlist> findByUser(User user);
+
+
 
 }
