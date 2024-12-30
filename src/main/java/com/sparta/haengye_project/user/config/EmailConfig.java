6c4125cfaba561a3a247c78fe95f0189ffe4037f
Choi Hyeok <114526859+Choi-Hyeok99@users.noreply.github.com>
2024-12-31 00:19:33 +0900

    delete : 기존 프로젝트주석

--- src/main/java/com/sparta/haengye_project/user/config/EmailConfig.java
+++ src/main/java/com/sparta/haengye_project/user/config/EmailConfig.java
@@ -1,40 +1,40 @@
-package com.sparta.haengye_project.user.config;
-
-import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-import java.util.Properties;
-
-@Configuration
-public class EmailConfig {
-
-    private final Dotenv dotenv = Dotenv.load(); // .env 파일 로드
-
-
-    @Bean
-    public JavaMailSender javaMailService() {
-
-        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-        javaMailSender.setHost(dotenv.get("MAIL_HOST"));// 메인 도메인 서버 주소 => 정확히는 smtp 서버 주소
-        javaMailSender.setUsername(dotenv.get("EMAIL_USERNAME"));// 네이버 아이디
-        javaMailSender.setPassword(dotenv.get("EMAIL_PASSWORD")); // 네이버 비밀번호
-        javaMailSender.setPort(465); // 메일 인증서버 포트
-        javaMailSender.setJavaMailProperties(getMailProperties()); // 메일 인증서버 정보 가져오기
-
-        return javaMailSender;
-    }
-
-    private Properties getMailProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("mail.transport.protocol", "smtp"); // 프로토콜 설정
-        properties.setProperty("mail.smtp.auth", "true"); // smtp 인증
-        properties.setProperty("mail.smtp.starttls.enable", "true"); // smtp strattles 사용
-        properties.setProperty("mail.debug", "true"); // 디버그 사용
-        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com"); // ssl 인증 서버는 smtp.naver.com
-        properties.setProperty("mail.smtp.ssl.enable","true"); // ssl 사용
-        return properties;
-    }
-}
+//package com.sparta.haengye_project.user.config;
+//
+//import io.github.cdimascio.dotenv.Dotenv;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.mail.javamail.JavaMailSenderImpl;
+//
+//import java.util.Properties;
+//
+//@Configuration
+//public class EmailConfig {
+//
+//    private final Dotenv dotenv = Dotenv.load(); // .env 파일 로드
+//
+//
+//    @Bean
+//    public JavaMailSender javaMailService() {
+//
+//        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+//        javaMailSender.setHost(dotenv.get("MAIL_HOST"));// 메인 도메인 서버 주소 => 정확히는 smtp 서버 주소
+//        javaMailSender.setUsername(dotenv.get("EMAIL_USERNAME"));// 네이버 아이디
+//        javaMailSender.setPassword(dotenv.get("EMAIL_PASSWORD")); // 네이버 비밀번호
+//        javaMailSender.setPort(465); // 메일 인증서버 포트
+//        javaMailSender.setJavaMailProperties(getMailProperties()); // 메일 인증서버 정보 가져오기
+//
+//        return javaMailSender;
+//    }
+//
+//    private Properties getMailProperties() {
+//        Properties properties = new Properties();
+//        properties.setProperty("mail.transport.protocol", "smtp"); // 프로토콜 설정
+//        properties.setProperty("mail.smtp.auth", "true"); // smtp 인증
+//        properties.setProperty("mail.smtp.starttls.enable", "true"); // smtp strattles 사용
+//        properties.setProperty("mail.debug", "true"); // 디버그 사용
+//        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com"); // ssl 인증 서버는 smtp.naver.com
+//        properties.setProperty("mail.smtp.ssl.enable","true"); // ssl 사용
+//        return properties;
+//    }
+//}
