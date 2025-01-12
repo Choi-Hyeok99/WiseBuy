a8f84e61f7575bbd385497f97d5568bd9d7eacd9
Choi-Hyeok99 <gur0709@naver.com>
2025-01-12 19:17:01 +0900

    delete : 기존 코드 삭제

--- src/main/java/com/sparta/haengye_project/user/exception/InvalidEmailCodeException.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.haengye_project.user.exception;
-
-public class InvalidEmailCodeException extends RuntimeException {
-    public InvalidEmailCodeException(String message) {
-        super(message);
-    }
-}
