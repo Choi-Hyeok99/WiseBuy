4c9ff68454d905544ed06a9ec6d80d015104ef89
Choi Hyeok <114526859+Choi-Hyeok99@users.noreply.github.com>
2025-01-12 19:17:01 +0900

    delete : 기존 코드 삭제

--- src/main/java/com/sparta/haengye_project/order/exception/OrderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.haengye_project.order.exception;
-
-public class OrderNotFoundException extends RuntimeException {
-    public OrderNotFoundException(String message){
-        super(message);
-    }
-}
--- src/main/java/com/sparta/haengye_project/product/exception/ForbiddenException.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.haengye_project.product.exception;
-
-public class ForbiddenException extends RuntimeException {
-    public ForbiddenException(String message){
-        super(message);
-    }
-}
